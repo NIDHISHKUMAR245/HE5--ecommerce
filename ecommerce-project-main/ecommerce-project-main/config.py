13c19f446e541f82c9381c829fe939c5adb3e165
NIDHISHKUMAR P <nidhish781@gmail.com>
2026-07-23 09:49:50 +0530

    Delete ecommerce-project-main/ecommerce-project-main directory

--- ecommerce-project-main/ecommerce-project-main/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'glownature-secret-key-12345')
-    MYSQL_HOST = os.environ.get('MYSQL_HOST', '127.0.0.1')
-    MYSQL_USER = os.environ.get('MYSQL_USER', 'root')
-    MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD', '')
-    MYSQL_DB = os.environ.get('MYSQL_DB', 'glownature_db')
